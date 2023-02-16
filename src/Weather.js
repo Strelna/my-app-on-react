27c71793479108aa9269a57a414166717316c14d
Strelna <aleesartshop@gmail.com>
2023-02-16 18:07:32 +0200

    h2 changes

--- src/Weather.js
+++ src/Weather.js
@@ -10,5 +10,5 @@ export default function Weather(props) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-    return <h2>Hello from Weather</h2>
+    return <h2>Hi from Weather</h2>
 }
