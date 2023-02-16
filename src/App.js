5a557b0039cbaadd6398116bd12919ff76d35c72
Strelna <aleesartshop@gmail.com>
2023-02-16 17:32:25 +0200

    City change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Weather city="Paris" />
+       <Weather city="London" />
       </header>
     </div>
   );
